bf5871691c5cb2d5f7fc3210be9cfdcc421cbe08
brettmt10 <brettmt10@gmail.com>
2024-11-27 16:32:18 -0500

    init schedule data

--- data_store.py
+++ data_store.py
@@ -1,7 +1,7 @@
 from api_handler import NHLHandler
 from dk_handler import DraftKingsDataHandler
 
-class DataStore():
+class NHLDataStore():
     def __init__(self, date: str = None):
         if date:
             self.nhl: NHLHandler = NHLHandler(date=date)
@@ -9,3 +9,9 @@ class DataStore():
             self.nhl: NHLHandler = NHLHandler()
             
         self.dk: DraftKingsDataHandler = DraftKingsDataHandler()
+        
+        self.scheduled_teams = self.nhl.schedule_handler.scheduled_teams()
+        self.schedule = self.nhl.schedule_handler.nhl_schedule()
+
+        
+    
