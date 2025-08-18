8b7698bae70a7032e315861456fb7087cc1a12ac
brettmt10 <brettmt10@gmail.com>
2025-08-17 20:32:09 -0400

    delete framework

--- src/app/web/nhl/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.hockey_teams_menu, name="hockey-menu"),
-    path("<str:team_abbrev>/", views.team_data, name="team-data"),
-]
